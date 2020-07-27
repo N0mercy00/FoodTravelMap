1d95455d1553c86a0825b9a66632f404c7103a6e
N0mercy00 <dkdhehesp@naver.com>
2020-07-27 15:58:22 +0900

    add) infobox for each color

--- data/data_G.js
+++ data/data_G.js
@@ -1,13 +1,13 @@
 var data_g = [ /*마커 하나하나의 데이터를 관리*/
     {
-        title: "서울",
-        content: '크레이지카츠',
+        title: "크레이지카츠",
+        content: '일식돈까스 특로스',
         score: "4.0",
         lat: 37.550370,
         lng: 126.909730
     }, {
-        title: "서울",
-        content: '카츠바이콘반',
+        title: "카츠바이콘반",
+        content: '일식돈까스',
         score: "4.2",
         lat: 37.524788,
         lng: 127.036460
