55fbf35e7a26416267c05ab5fc3d44f4a76fa990
N0mercy00 <dkdhehesp@naver.com>
2020-07-27 16:22:39 +0900

    data update color B

--- data/data_B.js
+++ data/data_B.js
@@ -1,15 +1,15 @@
 var data_b = [ /*마커 하나하나의 데이터를 관리*/
     {
-        title: "그레이샘플1",
-        content: '샘플데이터7',
-        score: "4.0",
-        lat: 28.542517,
-        lng: 126.947852
+        title: "깡통돌곱창",
+        content: '곱창',
+        score: "unknown",
+        lat: 37.739075,
+        lng: 127.044361
     }, {
-        title: "그레이샘플2",
-        content: '샘플데이터8',
+        title: "평양면옥",
+        content: '평양냉면',
         score: "4.2",
-        lat: 39.537208,
-        lng: 126.959004
+        lat: 37.730939,
+        lng: 127.047527
     }
 ]
